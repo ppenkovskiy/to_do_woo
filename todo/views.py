28a167a04b43852ab0c343f04e405fb2c9e974f7
p_penkovskiy <p.penkovskiy@gmail.com>
2022-01-09 16:05:54 +0300

    commit2

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def signupuser(request):
-    return render(request, 'todo/signupuser.html')
+    return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
