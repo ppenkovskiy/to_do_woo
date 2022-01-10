a56e4614b4cd7535f5d6c4f66c1f63927f2bc08f
p_penkovskiy <p.penkovskiy@gmail.com>
2022-01-10 17:15:12 +0300

    commit10

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
+from .models import Todo
+
 def home(request):
     return render(request, 'todo/home.html')
 
@@ -42,11 +44,15 @@ def createtodo(request):
     if request.method == "GET":
         return render(request, 'todo/createtodo.html', {'form':TodoForm()})
     else:
-        form = TodoForm(request.POST)
-        newtodo = form.save(commit=False)
-        newtodo.user = request.user
-        newtodo.save()
-        return redirect('currenttodos')
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error':'Bad data passed in. Try again.'})
 
 def currenttodos(request):
-    return render(request, 'todo/currenttodos.html')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos':todos})
